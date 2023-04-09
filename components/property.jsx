792f07ef73f0015dcd75b1d42dea2715f5e2a0d4
muhsin-web <muhsinabdulrasheed@gmail.com>
2023-04-09 12:01:01 +0100

    build project

--- components/property.jsx
+++ components/property.jsx
@@ -13,7 +13,7 @@ const  Property = ({property: {coverPhoto, price, rentFrequency, rooms, title, b
           <Link href={`/property/${externalID}`} passHref>
                <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
                     <Box>
-                         <Image src={coverPhoto ? coverPhoto.url: DefaultImage} width={420} height={100}/>
+                         <Image src={coverPhoto ? coverPhoto.url: DefaultImage} width={420} alt="property" height={100}/>
                     </Box>
                     <Box w='full'>
                          <Flex paddingTop='3' alignItems='center' justifyContent='space-between'>
--- src/pages/search.js
+++ src/pages/search.js
@@ -38,7 +38,7 @@ const Search = ({properties}) => {
           Properties {router.query.purpose}
          </Text>
          <Flex flexWrap="wrap">
-          {properties.map((property) => <Property property={property}/>)}
+          {properties.map((property) => <Property key={property.id} property={property}/>)}
          </Flex>
          {properties.length === 0 && (
           <Flex justifyContent="center" alignItems="center" flexDirection="column" marginTop="5" marginBottom="5">
